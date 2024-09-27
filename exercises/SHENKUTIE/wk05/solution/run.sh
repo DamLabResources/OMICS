e4c2ed0d1d369c4f8d9b036cde4f0435e7701256
shenkutie <ts3576@drexel.edu>
2024-09-27 19:39:15 -0400

    finished wk05 homework

--- exercises/SHENKUTIE/wk05/solution/run.sh
+++ exercises/SHENKUTIE/wk05/solution/run.sh
@@ -1,5 +1,5 @@
-#ln -s ../../../../content/wk05/data/multiplexed.fq
-#ln -s ../../../../content/wk05/data/sample_sheet.csv 
+ln -s ../../../../content/wk05/data/multiplexed.fq
+ln -s ../../../../content/wk05/data/sample_sheet.csv 
 wc -l sample_sheet.csv 
 cat sample_sheet.csv
 seqkit stats multiplexed.fq  | tee muxed.stats.tsv 
