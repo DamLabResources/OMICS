82c2fcb072dbc48c81a845992853603691cebb43
“scbarrera” <scbarrera@users.noreply.github.com>
2024-12-04 17:06:39 -0500

    updated count script - now counting protospacers

--- projects/scbarrera/count_protospacers.py
+++ projects/scbarrera/count_protospacers.py
@@ -3,13 +3,34 @@
 # print(f'NAME: {__name__}')
 
 from os.path import join
+from collections import Counter
 from glob import glob
 
 
+#glob = get list of base names 
 def main():
-    dir_x20 = "/home/jupyter-sb4476/projects/sbCRISPRSCREEN/02.reads_20nt/"
-    files_x20 = glob(join(dir_x20, 'SRR*_20nt.fasta'))
-    print(files_x20)
+    x20_pat = "/home/jupyter-sb4476/projects/sbCRISPRSCREEN/02.reads_20nt/SRR*_20nt.fasta"
+    # files_x20 = glob(join(x20_pat, 'SRR*_20nt.fasta'))
+    files_x20 = glob(x20_pat)
+    assert files_x20, f'NO FILES FOUND IN: {x20_pat}'
+
+    # collect all lines from all files
+    # fin means files in- defined below
+    lines = []
+    for fin in files_x20:
+        lines.extend(_read(fin))
+    assert lines, 'NO LINES FOUND'
+
+    # taking top 5
+    ctr = Counter(lines)
+    for val, qty in ctr.most_common(5):
+        print(f'{qty:6,} {val}', end='')
+
+
+# loop variable  = loop fin into read function
+def _read(fin):
+    with open(fin, encoding='utf8') as ifstream:
+        return ifstream.readlines()
 
 if __name__ == '__main__':
     main()
